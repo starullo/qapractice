b8a4d18dce68c937fced78bd109717d366d9af0c
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-05-17 11:25:52 -0500

    Added a new project and modified Results class to meet requirements of latest challenge

---
+++ FirstProject/src/com/qa/ListPractice.java
@@ -0,0 +1,15 @@
+package com.qa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ListPractice {
+	public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("wow");
+        System.out.println(words);
+        
+	}
+}
--- FirstProject/src/com/qa/Results.java
+++ FirstProject/src/com/qa/Results.java
@@ -2,12 +2,16 @@ package com.qa;
 
 public class Results {
 	
-	public static int physics = 100;
-	public static int chemistry = 105;
-	public static int biology = 92;
+	public static int physics = 150;
+	public static int chemistry = 90;
+	public static int biology = 50;
 	public static int total = physics + chemistry + biology;
 	public static double percentage = (total * 100) / 450;
 	public static String outOf = "/150";
+	public static double percentageToPass = 60.0;
+	public static double physicsPercentage = (physics * 100) / 150;
+	public static double chemistryPercentage = (chemistry * 100) / 150;
+	public static double biologyPercentage = (biology * 100) / 150;
 	
 	public static void main(String[] args) {
 		displayResults();
@@ -22,8 +26,41 @@ public class Results {
 		System.out.println("PHYSICS: " + physics + outOf);
 		System.out.println("CHEMISTRY: " + chemistry + outOf);
 		System.out.println("BIOLOGY: " + biology + outOf);
-		System.out.println("*TOTAL: " + total + "450" + "*");
-		System.out.println("***" + percentage() + "***");
+		System.out.println("\n*TOTAL: " + total + "/450" + "*");
+		System.out.println("***** " + percentage() + " *****");
+		if (percentage < percentageToPass || physicsPercentage < 60 || biologyPercentage < 60 || chemistryPercentage < 60) {
+			System.out.println("You have failed");
+		}
+		if (physicsPercentage < 60) {
+			String message = "Subjects failed: Physics";
+			if (chemistryPercentage < 60) {
+				message += ", Chemistry";
+			}
+			if (biologyPercentage < 60) {
+				message += ", Biology";
+			}
+			System.out.println(message);
+		} else if (chemistryPercentage < 60) {
+			String message = "Subjects failed: Chemistry";
+			if (physicsPercentage < 60) {
+				message += ", Physics";
+			}
+			if (biologyPercentage < 60) {
+				message += ", Biology";
+			}
+		} else if (biologyPercentage < 60) {
+			String message = "Subjects failed: Biology";
+			if (physicsPercentage < 60) {
+				message += ", Physics";
+			}
+			if (chemistryPercentage < 60) {
+				message += ", Chemistry";
+			}
+			System.out.println(message);
+		} else {
+			System.out.println("You have passed");
+		}
+		
 	}
 	
 	public static String percentage() {
